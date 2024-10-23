8436a92babc0fc3d79919e8548bffe2b03e9e6f6
ArchonWu <archonuwu@gmail.com>
2024-10-22 22:56:56 -0700

    alignment icon button

--- app/src/main/java/com/example/scrollcapturer/screenshotListScreen/ScreenshotListScreen.kt
+++ app/src/main/java/com/example/scrollcapturer/screenshotListScreen/ScreenshotListScreen.kt
@@ -1,9 +1,13 @@
 package com.example.scrollcapturer.screenshotListScreen
 
-import androidx.compose.foundation.layout.Column
+import android.net.Uri
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
@@ -15,21 +19,35 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import java.lang.reflect.Modifier
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun ScreenshotListScreen(navController: NavController) {
-    Column {
+    Box(
+        contentAlignment = Alignment.TopEnd,
+        modifier = Modifier
+            .fillMaxSize()
+//            .border(width = 2.dp, color = Color.Cyan)
+    ) {
         ExpandingMenu()
     }
 }
 
 @Composable
-fun ScreenshotEntry() {
-
+fun ScreenshotEntry(uri: Uri) {
+    Image(
+        painter = rememberAsyncImagePainter(model = uri),
+        contentDescription = "Screenshot",
+        contentScale = ContentScale.Crop,   // crop image to this size
+        modifier = Modifier
+            .fillMaxWidth()
+    )
 }
 
 @Composable
@@ -64,18 +82,20 @@ fun ExpandingMenu() {
         mutableStateOf(false)
     }
 
-    // the "3 dots" button
-    IconButton(onClick = { expanded = !expanded }) {
-        Icon(imageVector = Icons.Default.MoreVert, contentDescription = "More options")
-    }
-
-    // Display vertical column with 3 buttons
-    if (expanded) {
-        Column(
-        ) {
-            AddPictureButton()
-            RemovePictureButton()
-            StartStitchingButton()
+        // the "3 dots" button
+        IconButton(onClick = { expanded = !expanded }) {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "More options",
+            )
         }
-    }
+
+        // Display vertical column with 3 buttons
+        if (expanded) {
+            Column() {
+                AddPictureButton()
+                RemovePictureButton()
+                StartStitchingButton()
+            }
+        }
 }
