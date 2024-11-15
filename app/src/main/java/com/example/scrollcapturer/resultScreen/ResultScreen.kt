a03505c6eb1813e7bb5029d5e898f1c3e9264fd8
ArchonWu <archonuwu@gmail.com>
2024-11-14 21:54:19 -0800

    better button

--- app/src/main/java/com/example/scrollcapturer/resultScreen/ResultScreen.kt
+++ app/src/main/java/com/example/scrollcapturer/resultScreen/ResultScreen.kt
@@ -13,6 +13,7 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
@@ -22,6 +23,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
@@ -146,9 +148,14 @@ fun DialogTextField(
             ) {
                 // DismissButton
                 Button(
-                    onClick = { onDismiss() }
+                    onClick = { onDismiss() },
+                    shape = RoundedCornerShape(0.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.LightGray,
+                        contentColor = Color.Black
+                    )
                 ) {
-                    Text("CANCEL")
+                    Text("BACK")
                 }
 
                 // ConfirmButton
@@ -156,7 +163,12 @@ fun DialogTextField(
                     onClick = {
                         onSave()
                         onDismiss()
-                    }
+                    },
+                    shape = RoundedCornerShape(0.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.LightGray,
+                        contentColor = Color.Black
+                    )
                 ) {
                     Text("SAVE")
                 }
